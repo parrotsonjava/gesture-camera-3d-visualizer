2c6bf044ea699dfd9314e188b99e02b3e751385d
Thomas Endres <Thomas-Endres@gmx.de>
2016-09-02 19:46:02 +0200

    Updated RealSense to new SDK version 2016 R2

--- src/main/config.js
+++ src/main/config.js
@@ -8,7 +8,7 @@ require({
         'jquery-purl': 'js/vendor/jquery-purl',
 
         'leap': 'js/vendor/leap',
-        'intel-realSense': 'js/vendor/intel/realsense-5.0',
+        'intel-realSense': 'js/vendor/intel/realsense-10.0',
         'intel-realSense-promise': 'js/vendor/intel/promise-6.1.0',
         'intel-realSense-autobahn': 'js/vendor/intel/autobahn'
     },
